21f301fb13b55666856ee398f95410daa9289781
TOP-Z\riley <riley211375@gmail.com>
2024-01-26 14:24:22 +0800

    员工管理-员工导入模板下载

--- src/api/employee.js
+++ src/api/employee.js
@@ -19,3 +19,13 @@ export function exportEmployee() {
     responseType: 'blob'
   })
 }
+/**
+ * 下载员工导入模板
+ */
+export function getExportTemplate() {
+  return request({
+    url: '/sys/user/import/template',
+    // 使用blob接收二进制文件流
+    responseType: 'blob'
+  })
+}
--- src/views/employee/components/import-excel.vue
+++ src/views/employee/components/import-excel.vue
@@ -15,7 +15,7 @@
         >
         <div class="drop">
           <i class="el-icon-upload" />
-          <el-button type="text">下载导入模板</el-button>
+          <el-button type="text" @click="getTemplate">下载导入模板</el-button>
           <span>将文件拖到此处或
             <el-button type="text">点击上传</el-button>
           </span>
@@ -28,8 +28,10 @@
     </el-row>
   </el-dialog>
 </template>
-<script>
 
+<script>
+import FileSaver from 'file-saver'
+import { getExportTemplate } from '@/api/employee'
 export default {
   props: {
     showExcelDialog: {
@@ -38,7 +40,10 @@ export default {
     }
   },
   methods: {
-
+    async getTemplate() {
+      const data = await getExportTemplate()
+      FileSaver.saveAs(data, '员工模板.xlsx')
+    }
   }
 }
 </script>
