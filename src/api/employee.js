1513bb5fecb1d0b515dc921566191a65d2e110c4
TOP-Z\riley <riley211375@gmail.com>
2024-02-15 15:45:20 +0800

    员工详情-编辑员工-保存

--- src/api/employee.js
+++ src/api/employee.js
@@ -66,3 +66,13 @@ export function getEmployeeDetail(id) {
     url: `/sys/user/${id}`
   })
 }
+/**
+ * 更新员工
+ */
+export function updateEmployee(data) {
+  return request({
+    url: `/sys/user/${data.id}`,
+    method: 'put',
+    data
+  })
+}
--- src/views/employee/detail.vue
+++ src/views/employee/detail.vue
@@ -27,6 +27,7 @@
               <el-form-item label="手机" prop="mobile">
                 <el-input
                   v-model="userInfo.mobile"
+                  :disabled="!!$route.params.id"
                   size="mini"
                   class="inputW"
                 />
@@ -99,7 +100,7 @@
 </template>
 
 <script>
-import { addEmployee, getEmployeeDetail } from '@/api/employee'
+import { addEmployee, getEmployeeDetail, updateEmployee } from '@/api/employee'
 import SelectTree from './components/select-tree.vue'
 export default {
   components: { SelectTree },
@@ -148,9 +149,17 @@ export default {
     saveData() {
       this.$refs.userForm.validate(async isOK => {
         if (isOK) {
-          // 校验通过
-          await addEmployee(this.userInfo)
-          this.$message.success('新增员工成功')
+        // 编辑模式
+          if (this.$route.params.id) {
+            // 编辑模式
+            await updateEmployee(this.userInfo)
+            this.$message.success('更新员工成功')
+          } else {
+            // 新增模式
+            // 校验通过
+            await addEmployee(this.userInfo)
+            this.$message.success('新增员工成功')
+          }
           this.$router.push('/employee')
         }
       })
