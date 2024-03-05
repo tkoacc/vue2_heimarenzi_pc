93b59213d1b5320917214bf48f3e08f1a8c6e875
TOP-Z\riley <riley211375@gmail.com>
2024-03-05 13:33:55 +0800

    将路由改成history模式

--- src/router/index.js
+++ src/router/index.js
@@ -75,7 +75,7 @@ export const asyncRoutes = [departmentRouter,
   socialRouter
 ]
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
